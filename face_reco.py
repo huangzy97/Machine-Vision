0c89e21c34fe61ca009a3691003a900a2ff6ff75
huangzy97 <15651610660@wo.cn>
2019-07-08 14:14:39 +0800

    中文显示人名
    中文显示人名

---
+++ face_reco.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 13:55:57 2019
+
+@author: sbtithzy
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 10:07:33 2019
+@author: sbtithzy
+"""
+import dlib          # 人脸处理的库 Dlib
+import numpy as np   # 数据处理的库 numpy
+import cv2           # 图像处理的库 OpenCv
+import pandas as pd  # 数据处理的库 Pandas
+import time
+# 人脸识别模型，提取128D的特征矢量
+# Refer this tutorial: http://dlib.net/python/index.html#dlib.face_recognition_model_v1
+facerec = dlib.face_recognition_model_v1("data/data_dlib/dlib_face_recognition_resnet_model_v1.dat")
+# 计算两个128D向量间的欧式距离
+def return_euclidean_distance(feature_1, feature_2):
+    feature_1 = np.array(feature_1)
+    feature_2 = np.array(feature_2)
+    dist = np.sqrt(np.sum(np.square(feature_1 - feature_2)))
+    return dist
+# 处理存放所有人脸特征的 csv
+path_features_known_csv = "data/features_all.csv"
+csv_rd = pd.read_csv(path_features_known_csv, header=None)
+# 用来存放所有录入人脸特征的数组
+features_known_arr = []
+# 读取已知人脸数据
+for i in range(csv_rd.shape[0]):
+    features_someone_arr = []
+    for j in range(0, len(csv_rd.ix[i, :])):
+        features_someone_arr.append(csv_rd.ix[i, :][j])
+    features_known_arr.append(features_someone_arr)
+print("Faces in Database：", len(features_known_arr))
+# Dlib 检测器和预测器
+# The detector and predictor will be used
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
+# 创建 cv2 摄像头对象
+# cv2.VideoCapture(0) to use the default camera of PC,
+# and you can use local video name by use cv2.VideoCapture(filename)
+cap = cv2.VideoCapture(0)
+# 设置视频参数，propId 设置的视频参数，value 设置的参数值
+cap.set(3, 480)
+# cap.isOpened() 返回 true/false 检查初始化是否成功
+# when the camera is open
+# 中文显示名称
+import cv2
+import numpy
+from PIL import Image, ImageDraw, ImageFont
+def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
+    if (isinstance(img, numpy.ndarray)):  #判断是否OpenCV图片类型
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img)
+    fontText = ImageFont.truetype(
+        "font/simsun.ttc", textSize, encoding="utf-8")
+    draw.text((left, top), text, textColor, font=fontText)
+    return cv2.cvtColor(numpy.asarray(img), cv2.COLOR_RGB2BGR)
+while cap.isOpened():
+    flag, img_rd = cap.read()
+    kk = cv2.waitKey(1)
+    # 取灰度
+    img_gray = cv2.cvtColor(img_rd, cv2.COLOR_RGB2GRAY)
+    # 人脸数 faces
+    faces = detector(img_gray, 0)
+    # 待会要写的字体 font to write later
+    font = cv2.FONT_HERSHEY_COMPLEX
+    # 存储当前摄像头中捕获到的所有人脸的坐标/名字
+    # the list to save the positions and names of current faces captured
+    pos_namelist = []
+    name_namelist = []
+    # 按下 q 键退出
+    # press 'q' to exit
+    if kk == ord('q'):
+        break
+    else:
+        # 检测到人脸 when face detected
+        if len(faces) != 0:
+            # 获取当前捕获到的图像的所有人脸的特征，存储到 features_cap_arr
+            # get the features captured and save into features_cap_arr
+            features_cap_arr = []
+            for i in range(len(faces)):
+                shape = predictor(img_rd, faces[i])
+                features_cap_arr.append(facerec.compute_face_descriptor(img_rd, shape))
+            # 遍历捕获到的图像中所有的人脸
+            # traversal all the faces in the database
+            for k in range(len(faces)):
+                print("##### camera person", k+1, "#####")
+                # 让人名跟随在矩形框的下方
+                # 确定人名的位置坐标
+                # 先默认所有人不认识，是 unknown
+                name_namelist.append("unknown")
+                # 每个捕获人脸的名字坐标 the positions of faces captured
+                pos_namelist.append(tuple([faces[k].left(), int(faces[k].bottom() + (faces[k].bottom() - faces[k].top())/4)]))
+                # 对于某张人脸，遍历所有存储的人脸特征
+                e_distance_list = []
+                for i in range(len(features_known_arr)):
+                    # 如果 person_X 数据不为空
+                    if str(features_known_arr[i][0]) != '0.0':
+                        print("with person", str(i + 1), "the e distance: ", end='')
+                        e_distance_tmp = return_euclidean_distance(features_cap_arr[k], features_known_arr[i])
+                        print(e_distance_tmp)
+                        e_distance_list.append(e_distance_tmp)
+                    else:
+                        # 空数据 person_X
+                        e_distance_list.append(999999999)
+                # Find the one with minimum e distance
+                similar_person_num = e_distance_list.index(min(e_distance_list))
+                print("Minimum e distance with person", int(similar_person_num)+1)
+                if min(e_distance_list) < 0.4:                 
+                    name_namelist[k] = str("Person "+str(int(similar_person_num)+1))\
+                    .replace("Person 1", "中文")\###这里可以考虑做字典的映射，待改进的地方
+                    .replace("Person 2", "小明")\
+                    .replace("Person 3", "Ronnie")\
+                    .replace("Person 4", "Terry")\
+                    .replace("Person 5", "Wilson")
+                    print("May be person "+str(int(similar_person_num)+1))                   
+                else:
+                    print("Unknown person")
+                # 矩形框
+                for kk, d in enumerate(faces):
+                    # 绘制矩形框
+                    cv2.rectangle(img_rd, tuple([d.left(), d.top()]), tuple([d.right(), d.bottom()]), (0, 255, 255), 2)
+                print('\n')
+            # 在人脸框下面写人脸名字
+            for i in range(len(faces)):               
+                img_rd = cv2ImgAddText(img_rd, name_namelist[i], pos_namelist[i][0], pos_namelist[i][1], (0, 255, 255), 50)
+    img_rd = cv2ImgAddText(img_rd, "按'Q':退出", 20, 430, (84, 255, 159), 30)
+    cv2.putText(img_rd, "Face Recognition", (20, 40), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
+    img_rd = cv2ImgAddText(img_rd, "人脸：" + str(len(faces)), 20, 80, (0, 0, 255), 30)
+    # 窗口显示 show with opencv
+    cv2.imshow("camera", img_rd)
+# 释放摄像头 release camera
+cap.release()
+# 删除建立的窗口 delete all the windows
+cv2.destroyAllWindows()
